849d9ac6014adbc4ce7cbd63c00959ba4f259f84
Andrew Shen <andrewshen@gmail.com>
2021-09-02 22:30:02 -0400

    Formatting updates

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,17 +1,16 @@
 function appendCSS(url) {
   var head = document.head;
   var link = document.createElement('link');
-
   link.type = 'text/css';
   link.rel = 'stylesheet';
   link.href = url;
-
   head.appendChild(link);
 }
 
 appendCSS(
   'https://fonts.googleapis.com/css2?family=Newsreader:ital,wght@0,400;0,700;1,400;1,700&display=swap'
 );
+
 appendCSS(
   'https://fonts.googleapis.com/css2?family=Inter:wght@300;500&display=swap'
 );
