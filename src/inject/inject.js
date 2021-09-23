98bf0bd292b5e9703cafb68c9def2b4dc709dc7f
Andrew Shen <andrewshen@gmail.com>
2021-09-23 00:07:32 -0400

    Move TOC to sidebar nav

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -7,6 +7,14 @@ function appendCSS(url) {
   head.appendChild(link);
 }
 
+$(function () {
+  var nav = $('#article-nav').find('ul');
+  $('#article-nav').find('li')[0].remove();
+  $("a:contains('Bibliography')")[0].remove();
+  $("a:contains('Academic Tools')")[0].remove();
+  $('#toc').find('li').prependTo(nav);
+});
+
 appendCSS(
   'https://fonts.googleapis.com/css2?family=Newsreader:ital,wght@0,400;0,700;1,400;1,700&display=swap'
 );
