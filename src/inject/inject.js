20bd0955f030ec0a46cd1920d5896aa26f1116bb
Andrew Shen <andrewshen@gmail.com>
2021-09-30 17:00:21 -0400

    Improve print styling

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -7,6 +7,14 @@ function appendCSS(url) {
   head.appendChild(link);
 }
 
+function swapLogo(dark) {
+  if (dark) {
+    $('#site-logo').find('img').attr('src', 'https://i.imgur.com/eRpN6wC.png');
+  } else {
+    $('#site-logo').find('img').attr('src', '../../symbols/sep-man-red.png');
+  }
+}
+
 $(function () {
   var nav = $('#article-nav').find('ul');
   $('#article-nav').find('li')[0].remove();
@@ -14,13 +22,23 @@ $(function () {
   $("a:contains('Academic Tools')")[0].remove();
   $('#toc').find('li').prependTo(nav);
   $('input[type=search]').attr('placeholder', 'Type / to search SEP');
+
   if (
     window.matchMedia &&
     window.matchMedia('(prefers-color-scheme: dark)').matches
   ) {
-    // swap logo for dark mode
-    $('#site-logo').find('img').attr('src', 'https://i.imgur.com/eRpN6wC.png');
+    swapLogo(true);
   }
+
+  window
+    .matchMedia('(prefers-color-scheme: dark)')
+    .addEventListener('change', (event) => {
+      if (event.matches) {
+        swapLogo(true);
+      } else {
+        swapLogo(false);
+      }
+    });
 });
 
 $(document).keyup(function (e) {
