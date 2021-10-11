56ab12382d3b4319a73c1c853a2a362046b748b0
Andrew Shen <andrewshen@gmail.com>
2021-10-11 03:18:49 -0400

    Add manual theming

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -15,13 +15,82 @@ function swapLogo(dark) {
   }
 }
 
+function setDarkMode(dark) {
+  if (dark) {
+    $(document.body).addClass('dark');
+    swapLogo(true);
+    // change site to dark
+  } else {
+    $(document.body).removeClass('dark');
+    swapLogo(false);
+    // change site to light
+  }
+}
+
+function setTheme(e) {
+  if (e.matches) {
+    setDarkMode(true);
+  } else {
+    setDarkMode(false);
+  }
+}
+
+function updateTheme() {
+  const theme = localStorage.getItem('theme');
+  switch (theme) {
+    case null:
+      localStorage.setItem('theme', 'light');
+      break;
+    case 'auto':
+      if (
+        window.matchMedia &&
+        window.matchMedia('(prefers-color-scheme: dark)').matches
+      ) {
+        setDarkMode(true);
+      }
+      window
+        .matchMedia('(prefers-color-scheme: dark)')
+        .addEventListener('change', setTheme);
+      break;
+    case 'dark':
+      window
+        .matchMedia('(prefers-color-scheme: dark)')
+        .removeEventListener('change', setTheme);
+      setDarkMode(true);
+      break;
+    case 'light':
+      window
+        .matchMedia('(prefers-color-scheme: dark)')
+        .removeEventListener('change', setTheme);
+      setDarkMode(false);
+      break;
+  }
+}
+
+function updateThemeSelector() {
+  const theme = localStorage.getItem('theme');
+  switch (theme) {
+    case 'auto':
+      $('#theme-selector option[value="auto"]').attr('selected', 'selected');
+      break;
+    case 'dark':
+      $('#theme-selector option[value="dark"]').attr('selected', 'selected');
+      break;
+    case 'light':
+      $('#theme-selector option[value="light"]').attr('selected', 'selected');
+      break;
+  }
+}
+
 $(function () {
   const nav = $('#article-nav').find('ul');
-  $('#article-nav').find('li')[0].remove();
-  $("a:contains('Bibliography')")[0].remove();
-  $("a:contains('Academic Tools')")[0].remove();
-  $('#toc').find('li').prependTo(nav);
-  $('#toc').remove();
+  if ($('#article-nav').find('li')[0]) {
+    $('#article-nav').find('li')[0].remove();
+    $("a:contains('Bibliography')")[0].remove();
+    $("a:contains('Academic Tools')")[0].remove();
+    $('#toc').find('li').prependTo(nav);
+    $('#toc').remove();
+  }
 
   nav
     .find('li')
@@ -41,24 +110,50 @@ $(function () {
     })
     .css('margin-left', '40px');
 
-  $('input[type=search]').attr('placeholder', 'Type / to search SEP');
+  const selectContainer = document.createElement('div');
+  selectContainer.className = 'select-container';
 
-  if (
-    window.matchMedia &&
-    window.matchMedia('(prefers-color-scheme: dark)').matches
-  ) {
-    swapLogo(true);
-  }
+  const themeSelector = document.createElement('select');
+  themeSelector.setAttribute('id', 'theme-selector');
 
-  window
-    .matchMedia('(prefers-color-scheme: dark)')
-    .addEventListener('change', function (e) {
-      if (e.matches) {
-        swapLogo(true);
-      } else {
-        swapLogo(false);
+  const auto = document.createElement('option');
+  auto.setAttribute('value', 'auto');
+  auto.innerHTML = 'System';
+
+  const dark = document.createElement('option');
+  dark.setAttribute('value', 'dark');
+  dark.innerHTML = 'Dark';
+
+  const light = document.createElement('option');
+  light.setAttribute('value', 'light');
+  light.innerHTML = 'Light';
+
+  selectContainer.append(themeSelector);
+  $('#search').append(selectContainer);
+  $('#theme-selector').append(light);
+  $('#theme-selector').append(dark);
+  $('#theme-selector').append(auto);
+  $('#theme-selector').change(function () {
+    $('select option:selected').each(function () {
+      switch ($(this).attr('value')) {
+        case 'auto':
+          localStorage.setItem('theme', 'auto');
+          break;
+        case 'dark':
+          localStorage.setItem('theme', 'dark');
+          break;
+        case 'light':
+          localStorage.setItem('theme', 'light');
+          break;
       }
     });
+    updateTheme();
+  });
+
+  updateTheme();
+  updateThemeSelector();
+
+  $('input[type=search]').attr('placeholder', 'Type / to search SEP');
 });
 
 $(document).keyup(function (e) {
